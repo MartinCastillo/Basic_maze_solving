14b216c1ac318591f42200bae1d2178ab31346ee
Martin Castillo <martincastillo.mcg@gmail.com>
2019-08-03 20:32:19 -0400

    Se da la estructura de una clase robot, para mejor simulacion del problema

--- Real_time_map_construction/class_robot.py
+++ Real_time_map_construction/class_robot.py
@@ -5,15 +5,77 @@
 #from the envrioment that a robot would get, and return an action, forward, turn,rigth
 #turn left, backwards.
 #The envrioment can get all the robot's physical information
-import numpy as np
 class Robot:
-    def __init__(self,robot_shape,initial_position):
-        self.robot_shape = robot_shape
-        self.orientacion = []
-        self.position = np.array(initial_position)#Coorditates from the staring point
-        #always start in the left bottom corner
+    def __init__(self,_local,current_coord):
+        #La permutación antes descrita, cambiala si quieres variar la dirección inicial
+        #arriba derecha   abajo  izquierda
+        self._local = _local
+        #Si gira a la derecha _local = [4,1,2,3]
+        #2 * derecha = abajo ,_local = [3,4,1,2s]
+        #a la izquierda _local = [2,3,4,1] = 3 * derecha
+        #Llamaremos esta rotación a la drecha r
+        self.current_coord = current_coord
+        pass
 
-    def see_laterals(self,maze_format,ycoord,xcoord):
-        res_matrix = maze_format[ycoord-1:ycoord+2,xcoord-1:xcoord+2]
-        top distance
-        return(res_matrix)
+    def _avanzar(self,ycoord,xcoord,local_list):
+        """
+        Esta función avanza una celda en la direccón que la lista del ciclo
+        indique, luego retorna las nuevas cooordenadas.
+        """
+        #Dirección
+        dir = local_list.index(1)
+        #si es 0 apunta arriba,si es 1 es a la derecha,si es 2 abajo y 3 izquierda
+        new_ycoord=ycoord;new_xcoord=xcoord
+        if(dir==0):
+            new_ycoord = ycoord-1
+        elif(dir==1):
+            new_xcoord = xcoord+1
+        elif(dir==2):
+            new_ycoord = ycoord+1
+        elif(dir==3):
+            new_xcoord = xcoord-1
+        else:
+            print('Error en función "avanzar"')
+        return(new_ycoord,new_xcoord)
+
+    def r(self,local_list):
+        """
+        Una permutación el ciclo dado local_list Debe tener un largo 4"""
+        res = []
+        res.append(local_list[3]);res.append(local_list[0])
+        res.append(local_list[1]);res.append(local_list[2])
+        return(res)
+    def _consultar_YoX(self,num1,num2,ycoord,xcoord,maze_format):
+        """Consulta el eje Y o X dependiendo de la entrada, si num1=-1 consulta arriba
+        ,si num1=1 lo hace abajo , si num2=-1 derecha, num2=1 es izquierda,considere
+        que si consulta en un eje el otro num deve valer 0"""
+        return(True if(maze_format[ycoord+num1,xcoord+num2] !=999) else False)
+
+    def disponible(self,maze_format,ycoord,xcoord,local_list):
+        """Esta función consulta si la en la direccón indicada,coordenadas y en el mapa
+        hay una interferencia"""
+        dir = local_list.index(1)
+        #si es 0 apunta arriba,si es 1 es a la derecha,si es 2 abajo y 3 izquierda
+        if(dir==0):
+            return(self._consultar_YoX(-1,0,ycoord,xcoord,maze_format))
+        elif(dir==1):
+            return(self._consultar_YoX(0,1,ycoord,xcoord,maze_format))
+        elif(dir==2):
+            return(self._consultar_YoX(1,0,ycoord,xcoord,maze_format))
+        elif(dir==3):
+            return(self._consultar_YoX(0,-1,ycoord,xcoord,maze_format))
+        else:
+            print('Error en función "disponible"')
+
+    def avanzar(self,maze_format):
+        if(self.disponible(maze_format,self.current_coord[0],self.current_coord[1],self._local)):
+            current_coord=self._avanzar(self.current_coord[0],self.current_coord[1],self._local)
+        else:
+            print('Una pared se interpone, estas chocando con ella')
+
+    def gira_derecha(self):
+        self._local=self.r(self._local)
+        print('gira a la derecha')
+    def gira_izquierda(self):
+        self._local=self.r(self.r(self.r(self._local)))
+        print('gira a la izquierda')
--- Real_time_map_construction/main_algorithm_3.py
+++ Real_time_map_construction/main_algorithm_3.py
@@ -7,30 +7,20 @@ from matplotlib import pyplot as plt
 import cv2
 from time import sleep
 from random import randint
+
 #Local imports
 import gui_algorithm_3
+from class_robot import Robot
+
 #Global variables
 screen_size = 500
 block_size = 25
 cube_margin = 5
-delay=100
-current_coord=[12,8] #Coordenadas de inicio
+delay=100#delay entre frames
 font_size=2
-#Si esta variable es true cambiara de dirección cada cierta cantidad de numeros
-#dada por max_iter
-turning_direction = True #Esto es derecha, si es False es izquierda
-fliping = False
-max_iter = 4
-random_fliping = False #Giros aleatorios,para que funcione debes desactivar fliping
-#La permutación antes descrita, cambiala si quieres variar la dirección inicial
-#         arriba derecha   abajo  izquierda
-_local =  [   1 ,   2     ,   3      ,4]
-#Si gira a la derecha _local = [4,1,2,3]
-#2 * derecha = abajo ,_local = [3,4,1,2s]
-#a la izquierda _local = [2,3,4,1] = 3 * derecha
-#Llamaremos esta rotación a la drecha r
 
 #nxn matrix for the maze format, 0 is nothing in the place, 999 is wall,-1 is ending
+#The robot cant acces to this information, only throgth the lateral blocks
 maze_format=np.array(
 [[999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999],
  [999,  0,  0,  0,  0,999,  0,  0,  0,999,  0,  0,  0,999,999,999,999,999,999,999],
@@ -53,59 +43,8 @@ maze_format=np.array(
  [999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999],
  [999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999]]
     )
-action_register=[0]
-#see_laterals function
 
-def r(local_list):
-    """
-    Una permutación el ciclo dado local_list Debe tener un largo 4"""
-    res = []
-    res.append(local_list[3]);res.append(local_list[0])
-    res.append(local_list[1]);res.append(local_list[2])
-    return(res)
-
-def avanzar(ycoord,xcoord,local_list):
-    """
-    Esta función avanza una celda en la direccón que la lista del ciclo
-    indique, luego retorna las nuevas cooordenadas.
-    """
-    #Dirección
-    dir = local_list.index(1)
-    #si es 0 apunta arriba,si es 1 es a la derecha,si es 2 abajo y 3 izquierda
-    new_ycoord=ycoord;new_xcoord=xcoord
-    if(dir==0):
-        new_ycoord = ycoord-1
-    elif(dir==1):
-        new_xcoord = xcoord+1
-    elif(dir==2):
-        new_ycoord = ycoord+1
-    elif(dir==3):
-        new_xcoord = xcoord-1
-    else:
-        print('Error en función "avanzar"')
-    return(new_ycoord,new_xcoord)
-
-def _consultar_YoX(num1,num2,ycoord,xcoord):
-    """Consulta el eje Y o X dependiendo de la entrada, si num1=-1 consulta arriba
-    ,si num1=1 lo hace abajo , si num2=-1 derecha, num2=1 es izquierda,considere
-    que si consulta en un eje el otro num deve valer 0"""
-    return(True if(maze_format[ycoord+num1,xcoord+num2] !=999) else False)
-
-def disponible(maze_format,ycoord,xcoord,local_list):
-    """Esta función consulta si la en la direccón indicada,coordenadas y en el mapa
-    hay una interferencia"""
-    dir = local_list.index(1)
-    #si es 0 apunta arriba,si es 1 es a la derecha,si es 2 abajo y 3 izquierda
-    if(dir==0):
-        return(_consultar_YoX(-1,0,ycoord,xcoord))
-    elif(dir==1):
-        return(_consultar_YoX(0,1,ycoord,xcoord))
-    elif(dir==2):
-        return(_consultar_YoX(1,0,ycoord,xcoord))
-    elif(dir==3):
-        return(_consultar_YoX(0,-1,ycoord,xcoord))
-    else:
-        print('Error en función "disponible"')
+action_register=[]
 
 def see_laterals(maze_format,ycoord,xcoord):
     """
@@ -121,61 +60,43 @@ def see_laterals(maze_format,ycoord,xcoord):
     return(res_matrix)
 
 if(__name__=='__main__'):
-    iters=0
+    robot = Robot([1 ,2,3,4],[12,8])
     while(True):
-            if(maze_format[current_coord[0],current_coord[1]] == -1):
+            #Si llegas
+            if(maze_format[robot.current_coord[0],robot.current_coord[1]] == -1):
                 break
-            ret, map = gui_adaptada.draw_map(maze_format,screen_size,block_size,
-                corner_text=str(iters),corner_text_size=font_size,corner_text_color=(100,100,255)
+            
+            #robot.avanza(maze_format)
+            #robot.gira_derecha()
+            #robot.gira_izquierda()
+            #Aquí se puede hacer un ciclo del robot, puede realizar las Acciones
+            #anteriores, y acceder a la informacion de res matrix
+            ####################################################################
+            ####################################################################
+            #Analizar alrededores, res matrix es la unica informacion del entorno
+            #(maze_format)que puede recibir, si se quiere orientar, va a tener
+            #que hacerlo con un mapa inerno
+            res_matrix=see_laterals(maze_format,robot.current_coord[0],robot.current_coord[1])
+            robot.avanzar(maze_format)
+            robot.gira_derecha()
+
+            ####################################################################
+            ####################################################################
+            #Dibuja en pantalla
+            ret, map = gui_algorithm_3.draw_map(maze_format,screen_size,block_size,
+                corner_text=str('text'),corner_text_size=font_size,corner_text_color=(100,100,255)
                     )
-            map = gui_adaptada.draw_cube(map,current_coord[0],current_coord[1],
-                _local,cube_margin,block_size
+            map = gui_algorithm_3.draw_cube(map,robot.current_coord[0],robot.current_coord[1],
+                robot._local,cube_margin,block_size
                     )
             cv2.imshow('1',map)
             k = cv2.waitKey(delay)
             if(k==ord('q')):
                 break
-            #print('iters+=1')
-            #Analizar alrededores
-            res_matrix=see_laterals(maze_format,current_coord[0],
-                current_coord[1]
-                    )
-            #Si en la direccón de _local no hay nada avanza
-            if(disponible(maze_format,current_coord[0],current_coord[1],_local)):
-                current_coord=avanzar(current_coord[0],current_coord[1],_local)
-                #Esta condición es para no incluir innecesarios 'Avanza'
-                if(action_register[::-1][0]!='Avanza'):
-                    action_register.append('Avanza')
-            #Si no, gira dependiendo de la variable global turning_direction
-            else:
-                iters+=1
-                if(turning_direction):
-                    _local=r(_local)
-                    action_register.append('Gira derecha')
-                else:
-                    _local=r(r(r(_local)))
-                    action_register.append('Gira izquierda')
-                if(iters%max_iter==0)and(fliping):
-                    turning_direction = not(turning_direction)
-                    action_register.append('cambio turning_direction a {}'.format(
-                        turning_direction
-                            ))
-                #Cambia de dirección de forma aleatoria(Funcionaba muy mal)
-                if(random_fliping):
-                    turning_direction = [True,False][randint(0,1)]
-                    action_register.append('cambio turning_direction a {}'.format(
-                        turning_direction
-                            ))
-    ret, map = gui_adaptada.draw_map(maze_format,screen_size,block_size,
-        corner_text=(str(iters)+' giros'),corner_text_size=font_size
-            )
-    map = gui_adaptada.draw_cube(map,current_coord[0],current_coord[1],
-        _local,cube_margin,block_size
-            )
+    #Terimna programa#
     cv2.imshow('1',map)
     k = cv2.waitKey(0)
     if(k==ord('q')):
         cv2.destroyAllWindows()
-        print('Iterations: {}'.format(iters),'---------------------------')
         print('Acciones: {}'.format(action_register),'---------------------------')
     pass
